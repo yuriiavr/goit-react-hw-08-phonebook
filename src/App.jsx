7e81f4e7c7f519dc8ccec960ddb7467c4ef2be15
yuriiavr <yurii.avramets@gmail.com>
2022-12-26 10:36:12 +0300

    321312

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ export default function App() {
   ) : (
     <Routes>
       <Route path="/" element={<AppBar />}>
-        <Route index element={<ContactsPage />} />
+        <Route index element={<HomePage />} />
         <Route
           path="contacts"
           element={
