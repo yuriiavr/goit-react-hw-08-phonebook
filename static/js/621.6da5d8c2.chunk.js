5cbaa324a3994d99b1801e04899116460c13ba91
yuriiavr <94492796+yuriiavr@users.noreply.github.com>
2023-01-26 18:18:29 +0000

    Deploying to gh-pages from @ yuriiavr/goit-react-hw-08-phonebook@cb9031c51eeb11e2df286ecf98339da4152e53c3 🚀

--- static/js/621.6da5d8c2.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[621],{4621:function(e,r,s){s.r(r),s.d(r,{default:function(){return c}});var t=s(184);function c(){return(0,t.jsx)(t.Fragment,{children:(0,t.jsxs)("section",{children:[(0,t.jsx)("h1",{children:"Home Page"}),(0,t.jsx)("img",{src:"https://cdn.dribbble.com/users/78806/screenshots/6746963/ios_13_caller_4-2_4x.png?compress=1&resize=1000x750&vertical=top",alt:""})]})})}}}]);
-//# sourceMappingURL=621.6da5d8c2.chunk.js.map
