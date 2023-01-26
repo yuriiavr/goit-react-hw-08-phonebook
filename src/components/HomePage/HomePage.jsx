f632b6577a54a1f694abe3ef2560702160e690e8
yuriiavr <yurii.avramets@gmail.com>
2023-01-26 19:48:38 +0300

    Update HomePage.jsx

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -3,6 +3,7 @@ export default function HomePage() {
       <>
         <section>
           <h1>Home Page</h1>
+          <a href="https://cdn.dribbble.com/users/78806/screenshots/6746963/ios_13_caller_4-2_4x.png?compress=1&resize=1000x750&vertical=top"></a>
         </section>
       </>
     );
