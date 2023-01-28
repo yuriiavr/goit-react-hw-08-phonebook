49d523562e74ef8f352d255b174552af5d1d7481
yuriiavr <yurii.avramets@gmail.com>
2023-01-28 17:45:13 +0300

    2312

--- src/redux/auth/authSelectors.js
+++ src/redux/auth/authSelectors.js
@@ -2,7 +2,7 @@ const getIsLoggedIn = state => state.auth.isLoggedIn;
 
 const getUserName = state => state.auth.user.name;
 
-
+const getEmail = state => state.auth.user.email;
 
 const getIsFetchingCurrent = state => state.auth.isFetchingCurrentUser;
 
