d3632bafbe8949ef619c336ca8861fe864914b6c
yuriiavr <yurii.avramets@gmail.com>
2023-02-01 00:16:27 +0300

    Update authSelectors.js

--- src/redux/auth/authSelectors.js
+++ src/redux/auth/authSelectors.js
@@ -2,7 +2,7 @@ const getIsLoggedIn = state => state.auth.isLoggedIn;
 
 const getUserName = state => state.auth.user.name;
 
-const getEmail = state => state.auth.user.email;
+const getEmail = state => state.auth.user;
 
 const getIsFetchingCurrent = state => state.auth.isFetchingCurrentUser;
 
